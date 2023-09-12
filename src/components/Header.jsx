b21e6d16a19e5497068b92f3305086dd278c9988
Sarang Patel <sarangsakariya1999@gmail.com>
2023-09-12 11:39:10 +0530

    Digital Involep

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
                             
                             
                         </ul>
-                    <NavLink className="navbar-brand mx-auto fw-bold" to="/">APPLE MART</NavLink>
+                    <NavLink className="navbar-brand mx-auto fw-bold" to="/">Premium Store</NavLink>
                     <Login/>
                     <Signup/>
                     <CartBtn/>
